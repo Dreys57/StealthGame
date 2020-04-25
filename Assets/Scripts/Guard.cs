3d3ec48d78142d76241aefe66a2aa039c8e00230
Dreys57 <skriosdreys@gmail.com>
2020-04-25 19:43:54 +0200

    Added controller mapping as well as basic AI that follows a simple looping path

---
+++ Assets/Scripts/Guard.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+    [SerializeField] private Transform pathHolder;
+
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float waitTime = 0.3f;
+    
+    void Start()
+    {
+        Vector3[] waypoints = new Vector3[pathHolder.childCount];
+
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = pathHolder.GetChild(i).position;
+            waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
+        }
+
+        StartCoroutine(FollowPath(waypoints));
+    }
+
+    IEnumerator FollowPath(Vector3[] waypoints)
+    {
+        transform.position = waypoints[0];
+
+        int targetWaypointIndex = 1;
+        Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+
+        while (true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
+
+            if (transform.position == targetWaypoint)
+            {
+                targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
+                targetWaypoint = waypoints[targetWaypointIndex];
+                
+                yield return new WaitForSeconds(waitTime);
+            }
+
+            yield return null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 startPosition = pathHolder.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+        
+        foreach (Transform waypoint in pathHolder)
+        {
+            Gizmos.DrawSphere(waypoint.position, 0.3f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+        
+        Gizmos.DrawLine(previousPosition, startPosition);
+    }
+}
